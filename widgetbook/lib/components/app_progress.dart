111565302d156c2ba572be98263007a53858a0fb
codeswot <elmubarak333@gmail.com>
2026-06-08 10:02:17 +0100

    Remove Windows runner and Flutter CMake configuration files
    - Deleted CMakeLists.txt for Flutter Windows, which managed build steps and configurations.
    - Removed generated_plugin_registrant.cc and generated_plugin_registrant.h files that registered plugins for the Flutter application.
    - Eliminated generated_plugins.cmake, which listed Flutter plugins and their dependencies.
    - Removed the entire runner directory, including main application files, resources, and utility functions for the Windows runner.
    - This cleanup is part of a larger refactor to streamline the Flutter Windows integration and remove unnecessary files.

--- widgetbook/lib/components/app_progress.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-import 'package:zapbook/widgets/app_progress.dart';
-
-@widgetbook.UseCase(name: 'Default', type: AppProgress)
-Widget buildDefaultAppProgressUseCase(BuildContext context) {
-  return const AppProgress(value: 0.65);
-}
-
